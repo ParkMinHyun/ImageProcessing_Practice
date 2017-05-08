cc6aee2d82c87c8363d13d716ec08180353c0484
박민현 <algusl92@naver.com>
2017-05-08 14:43:47 +0900

    산술 연산 기능 완료2

--- ImageProcessing/ImageProcessing/Practicecpp.cpp
+++ ImageProcessing/ImageProcessing/Practicecpp.cpp
@@ -1,15 +1,90 @@
-#include <opencv\cv.h>
-#include <opencv\highgui.h>
+#include <opencv/cv.h>
+#include <opencv/cxcore.h>
+#include <opencv/highgui.h>
 
-int main()
-{
-	IplImage *image = cvLoadImage("lena.jpg", CV_LOAD_IMAGE_COLOR);
+#define SUM_CONSTANT 50
+#define SUB_CONSTANT 50
+#define MUL_CONSTANT 5
+#define DIV_CONSTANT 5
 
-	cvShowImage("Test Viewer", image);
+int main() {
+	int i, j;
 
-	cvWaitKey();
-	cvDestroyWindow("Test Viewer");
-	cvReleaseImage(&image);
+	IplImage* inputimage = cvLoadImage("lena.jpg", CV_LOAD_IMAGE_GRAYSCALE); // 이미지불러오기, 초기이미지
+	IplImage* sumimage = cvCreateImage(cvGetSize(inputimage), inputimage->depth, inputimage->nChannels); // 산술연산 이미지생성
+	IplImage* subimage = cvCreateImage(cvGetSize(inputimage), inputimage->depth, inputimage->nChannels);
+	IplImage* mulimage = cvCreateImage(cvGetSize(inputimage), inputimage->depth, inputimage->nChannels);
+	IplImage* divimage = cvCreateImage(cvGetSize(inputimage), inputimage->depth, inputimage->nChannels);
+
+	CvScalar pixelValue, temp;
+
+	for (i = 0; i < inputimage->height; i++) {
+		for (j = 0; j < inputimage->width; j++) {
+			pixelValue = cvGet2D(inputimage, i, j); // 색 반환
+													//덧셈 연산
+			if (pixelValue.val[0] + SUM_CONSTANT >= 255) { // 255가 맥스값
+				temp.val[0] = 255;
+				cvSet2D(sumimage, i, j, temp); // 색 설정
+			}
+			else {
+				temp.val[0] = pixelValue.val[0] + SUM_CONSTANT;
+				cvSet2D(sumimage, i, j, temp);
+			}
+			//뺄셈 연산
+			if (pixelValue.val[0] - SUB_CONSTANT < 0) { // 0이 최소값
+				temp.val[0] = 0;
+				cvSet2D(subimage, i, j, temp);
+			}
+			else {
+				temp.val[0] = pixelValue.val[0] - SUB_CONSTANT;
+				cvSet2D(subimage, i, j, temp);
+			}
+			//곱셈 연산
+			if (pixelValue.val[0] * MUL_CONSTANT >= 255) {
+				temp.val[0] = 255;
+				cvSet2D(mulimage, i, j, temp);
+			}
+			else {
+				temp.val[0] = pixelValue.val[0] * MUL_CONSTANT;
+				cvSet2D(mulimage, i, j, temp);
+			}
+			//나눗셈 연산
+			if (pixelValue.val[0] / DIV_CONSTANT < 0) {
+				temp.val[0] = 0;
+				cvSet2D(divimage, i, j, temp);
+			}
+			else {
+				temp.val[0] = pixelValue.val[0] / DIV_CONSTANT;
+				cvSet2D(divimage, i, j, temp);
+			}
+		}
+	}
+
+	cvNamedWindow("input image", CV_WINDOW_AUTOSIZE); // 창 생성
+	cvNamedWindow("sum image", CV_WINDOW_AUTOSIZE);
+	cvNamedWindow("sub image", CV_WINDOW_AUTOSIZE);
+	cvNamedWindow("mul image", CV_WINDOW_AUTOSIZE);
+	cvNamedWindow("div image", CV_WINDOW_AUTOSIZE);
+
+	cvShowImage("input image", inputimage); // 창과 이미지 연결
+	cvShowImage("sum image", sumimage);
+	cvShowImage("sub image", subimage);
+	cvShowImage("mul image", mulimage);
+	cvShowImage("div image", divimage);
+
+	cvWaitKey(); // 키 엽력 대기
+
+	cvDestroyWindow("inputimage"); // 창 부수기
+	cvDestroyWindow("sumimage");
+	cvDestroyWindow("subimage");
+	cvDestroyWindow("mulimage");
+	cvDestroyWindow("divimage");
+
+	cvReleaseImage(&inputimage); // 포인터 해제
+	cvReleaseImage(&sumimage);
+	cvReleaseImage(&subimage);
+	cvReleaseImage(&mulimage);
+	cvReleaseImage(&divimage);
 
 	return 0;
 }
