737f4922449b2279a7676bb2b2c1dabebbde6961
박민현 <algusl92@naver.com>
2017-05-09 18:46:16 +0900

    블러링 실습

--- ImageProcessing/ImageProcessing/Practicecpp.cpp
+++ ImageProcessing/ImageProcessing/Practicecpp.cpp
@@ -716,7 +716,7 @@ int main() {
 
 	return 0;
 }*/
-/*히스토그램 명세화*/
+/*히스토그램 명세화
 #include <opencv/cv.h>
 #include <opencv/highgui.h> // opencv 라이브러리
 
@@ -853,3 +853,61 @@ void Show_histogram(IplImage* img, char* imgWindowName, char* histogramWindowNam
 	cvShowImage(imgWindowName, inputImage);
 	cvShowImage(histogramWindowName, histogramImage);
 }
+*/
+/*블러링*/
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+IplImage* ConvolutionProcess(IplImage* inputImage, double Mask[3][3]);
+
+int main() {
+	IplImage* inputImage = cvLoadImage("lena.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	IplImage* ResultImage = NULL;
+
+	double BlurringMask[3][3] = { { 1 / 9.,1 / 9.,1 / 9. },{ 1 / 9.,1 / 9., 1 / 9. },{ 1 / 9.,1 / 9.,1 / 9. } };
+
+	ResultImage = ConvolutionProcess(inputImage, BlurringMask);
+
+	cvShowImage("input Image", inputImage);
+	cvShowImage("Result Image", ResultImage);
+
+	cvWaitKey();
+	cvDestroyAllWindows();
+	cvReleaseImage(&inputImage);
+	cvReleaseImage(&ResultImage);
+}
+
+IplImage* ConvolutionProcess(IplImage* inputImage, double Mask[3][3]) {
+	IplImage* tempinputImage = cvCreateImage(cvSize(inputImage->width + 2, inputImage->height + 2), IPL_DEPTH_8U, 1);
+	IplImage* outputImage = cvCreateImage(cvGetSize(inputImage), IPL_DEPTH_8U, 1);
+
+	int i, j, n, m;
+	double Sum = 0.0;
+	CvScalar tempScalar;
+
+	cvSetZero(tempinputImage); // 경계처리는 0을 삽입, temp이미지 검은색으로 채움
+
+	for (i = 0; i < inputImage->height; i++) { // temp 이미지에 인풋 이미지 복사
+		for (j = 0; j < inputImage->width; j++) {
+			cvSet2D(tempinputImage, i + 1, j + 1, cvGet2D(inputImage, i, j));
+		}
+	}
+
+	for (i = 0; i < inputImage->height; i++) { // 컨벌루션 연산
+		for (j = 0; j < inputImage->width; j++) {
+			for (n = 0; n < 3; n++) {
+				for (m = 0; m < 3; m++) { // 마스크와 연산하면서 Sum에 누적
+					tempScalar = cvGet2D(tempinputImage, i + n, j + m);
+					Sum += Mask[n][m] * tempScalar.val[0];
+				}
+			}
+			cvSet2D(outputImage, i, j, cvScalar(Sum)); // 결과값을 아웃풋 이미지에 넣음
+			Sum = 0.0;
+		}
+	}
+
+	cvReleaseImage(&tempinputImage);
+
+	return outputImage;
+}
+
