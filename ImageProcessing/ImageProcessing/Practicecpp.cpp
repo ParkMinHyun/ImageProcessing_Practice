37b4863e7eef431724b05feaad15c0d7200f9363
박민현 <algusl92@naver.com>
2017-05-09 18:44:48 +0900

    도전과제 1 Image 합성

--- ImageProcessing/ImageProcessing/Practicecpp.cpp
+++ ImageProcessing/ImageProcessing/Practicecpp.cpp
@@ -562,6 +562,7 @@ void Show_histogram(IplImage* img, char* imgWindowName, char* histogramWindowNam
 	cvShowImage(histogramWindowName, histogramImage);
 }
 */
+/*히스토그램 평활화
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 
@@ -666,4 +667,51 @@ void Show_histogram(IplImage* img, char* imgWindowName, char* histogramWindowNam
 
 	cvShowImage(imgWindowName, inputImage);
 	cvShowImage(histogramWindowName, histogramImage);
+}
+*/
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+int main() {
+	IplImage* moonsu = cvLoadImage("moonsu.jpg", CV_LOAD_IMAGE_COLOR); // 기존 이미지 로드
+	IplImage* heart = cvLoadImage("heart.jpg", CV_LOAD_IMAGE_COLOR); // 합성할 이미지 로드
+	IplImage* mix = cvCreateImage(cvGetSize(moonsu), moonsu->depth, moonsu->nChannels); // 합성된 이미지 생성
+
+	int i, j;
+	CvScalar pixelValue, temp;
+
+	for (i = 0; i < moonsu->height; i++) {
+		for (j = 0; j < moonsu->width; j++) {
+			pixelValue = cvGet2D(heart, i, j); // 합성할 이미지의 색 얻기
+
+			if (pixelValue.val[0] < 240) {
+				temp = cvGet2D(heart, i, j); // 합성할 이미지 보다 어두우면 그 색을 설정
+				cvSet2D(mix, i, j, temp);
+			}
+			else {
+				temp = cvGet2D(moonsu, i, j); // 그외는 원본 값
+				cvSet2D(mix, i, j, temp);
+			}
+		}
+	}
+
+	cvNamedWindow("moonsu image", CV_WINDOW_AUTOSIZE);
+	cvNamedWindow("heart image", CV_WINDOW_AUTOSIZE);
+	cvNamedWindow("mix image", CV_WINDOW_AUTOSIZE);
+
+	cvShowImage("moonsu image", moonsu);
+	cvShowImage("heart image", heart);
+	cvShowImage("mix image", mix);
+
+	cvWaitKey();
+
+	cvDestroyWindow("moonsu image");
+	cvDestroyWindow("heart image");
+	cvDestroyWindow("mix image");
+
+	cvReleaseImage(&moonsu);
+	cvReleaseImage(&heart);
+	cvReleaseImage(&mix);
+
+	return 0;
 }
