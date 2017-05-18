150bd7568cda93340872035c51df59601ca5ae3a
DESKTOP-3RFQDQR\ParkMinHyun <algusl92@naver.com>
2017-05-18 15:04:54 +0900

    라플라시안 에지 검출 실습

--- ImageProcessingPractice2/ImageProcessingPractice2/PracticeImageProcessing.cpp
+++ ImageProcessingPractice2/ImageProcessingPractice2/PracticeImageProcessing.cpp
@@ -1,4 +1,4 @@
-/*유사 연산자 기법*/
+/*유사 연산자 기법
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 
@@ -54,6 +54,68 @@ IplImage* HomogenProcess(IplImage* inputImage) {
 			}
 			cvSet2D(outputImage, i, j, cvScalar(Max)); // 결과값을 아웃풋 이미지에 넣음
 			Max = 0.0;
+		}
+	}
+	cvReleaseImage(&tempinputImage);
+
+	return outputImage;
+}
+*/
+/*라플라시안*/
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+IplImage* ConvolutionProcess(IplImage* inputImage, double Mask[3][3]);
+
+int main() {
+	IplImage* inputImage = cvLoadImage("lena.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	IplImage* LaplaImage_1 = NULL;
+	IplImage* LaplaImage_2 = NULL;
+
+	double LaplaMask_1[3][3] = { { 0.,-1.,-0. },{ -1.,4.,-1. },{ 0.,-1.,0. } }; // 라플라시안
+	double LaplaMask_2[3][3] = { { -1.,-1.,-1. },{ -1.,8.,-1. },{ -1.,-1.,-1. } };
+
+	LaplaImage_1 = ConvolutionProcess(inputImage, LaplaMask_1);
+	LaplaImage_2 = ConvolutionProcess(inputImage, LaplaMask_2);
+
+	cvShowImage("input Image", inputImage);
+	cvShowImage("LaplaImage_1 Image", LaplaImage_1);
+	cvShowImage("LaplaImage_2 Image", LaplaImage_2);
+
+	cvWaitKey();
+	cvDestroyAllWindows();
+
+	cvReleaseImage(&LaplaImage_1);
+	cvReleaseImage(&LaplaImage_2);
+	cvReleaseImage(&inputImage);
+}
+
+IplImage* ConvolutionProcess(IplImage* inputImage, double Mask[3][3]) {
+	IplImage* tempinputImage = cvCreateImage(cvSize(inputImage->width + 2, inputImage->height + 2), IPL_DEPTH_8U, 1);
+	IplImage* outputImage = cvCreateImage(cvGetSize(inputImage), IPL_DEPTH_8U, 1);
+
+	int i, j, n, m;
+	double Sum = 0.0;
+	CvScalar tempScalar;
+
+	cvSetZero(tempinputImage); // 경계처리는 0을 삽입, temp이미지 검은색으로 채움
+
+	for (i = 0; i < inputImage->height; i++) { // temp 이미지에 인풋 이미지 복사
+		for (j = 0; j < inputImage->width; j++) {
+			cvSet2D(tempinputImage, i + 1, j + 1, cvGet2D(inputImage, i, j));
+		}
+	}
+
+	for (i = 0; i < inputImage->height; i++) { // 컨벌루션 연산
+		for (j = 0; j < inputImage->width; j++) {
+			for (n = 0; n < 3; n++) {
+				for (m = 0; m < 3; m++) { // 마스크와 연산하면서 Sum에 누적
+					tempScalar = cvGet2D(tempinputImage, i + n, j + m);
+					Sum += Mask[n][m] * tempScalar.val[0];
+				}
+			}
+			cvSet2D(outputImage, i, j, cvScalar(Sum)); // 결과값을 아웃풋 이미지에 넣음
+			Sum = 0.0;
 		}
 	}
 	cvReleaseImage(&tempinputImage);
