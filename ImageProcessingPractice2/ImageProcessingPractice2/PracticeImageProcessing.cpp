82338bedfea0987c3d676636a34ff9617dfd0101
DESKTOP-3RFQDQR\ParkMinHyun <algusl92@naver.com>
2017-05-22 13:40:17 +0900

    사진 이동 실습

--- ImageProcessingPractice2/ImageProcessingPractice2/PracticeImageProcessing.cpp
+++ ImageProcessingPractice2/ImageProcessingPractice2/PracticeImageProcessing.cpp
@@ -179,7 +179,7 @@ IplImage* ConvolutionProcess(IplImage* inputImage, double Mask[3][3]) {
 	return outputImage;
 }
 */
-/*로버츠 마스크*/
+/*로버츠 마스크
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 
@@ -255,4 +255,36 @@ IplImage* ConvolutionProcess(IplImage* inputImage, double Mask[3][3]) {
 	cvReleaseImage(&tempinputImage);
 
 	return outputImage;
+}
+*/
+/*이동*/
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+#define H_POS 30
+#define W_POS 130
+
+int main() {
+	IplImage* inputImage = cvLoadImage("lena.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	IplImage* outputImage = cvCreateImage(cvSize(inputImage->width, inputImage->height), inputImage->depth, inputImage->nChannels);
+
+	int i, j;
+
+	cvSetZero(outputImage);
+
+	for (i = 0; i < inputImage->height - H_POS; i++) {
+		for (j = 0; j < inputImage->width - W_POS; j++) {
+			cvSet2D(outputImage, i + H_POS, j + W_POS, cvGet2D(inputImage, i, j));
+		}
+	}
+
+	cvShowImage("input Image", inputImage);
+	cvShowImage("Output Image", outputImage);
+	cvWaitKey();
+
+	cvDestroyAllWindows();
+	cvReleaseImage(&inputImage);
+	cvReleaseImage(&outputImage);
+
+	return 0;
 }
